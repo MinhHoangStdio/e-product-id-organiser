33c790587b42e6e9d2b69281cb7e34493d01e2cf
nhat_minh <hnnminh24112000@gmail.com>
2023-09-08 15:39:25 +0700

    Update: config env

--- src/api/organizer/index.ts
+++ src/api/organizer/index.ts
@@ -6,7 +6,7 @@ const organizerApi = {
     return axiosClient.post(url, params);
   },
   getListOrganizer(params: any) {
-    const url = `/organizations`;
+    const url = `/organizations/`;
     return axiosClient.get(url, { params });
   },
   getDetailOrganizer(id: any) {
