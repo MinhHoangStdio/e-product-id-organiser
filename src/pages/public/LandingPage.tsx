a627db6be57ae6833081cd5b6e398a6aa7ab292a
nhat_minh <hnnminh24112000@gmail.com>
2023-09-11 09:17:36 +0700

    Update: Update env

---
+++ src/pages/public/LandingPage.tsx
@@ -0,0 +1,24 @@
+// import Navbar from "../components/layout/Navbar";
+// import Hero from "../components/home/Hero";
+// import Motivation from "../components/home/Motivation";
+// import Blog from "../components/home/Blog";
+// import Footer from "../components/layout/Footer";
+
+// const Home = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <div className="relative overflow-hidden">
+//         <div className="hidden lg:block w-full h-full absolute">
+//           <div className="bg-image-mockups absolute z-20 w-full h-full bg-no-repeat bg-auto bg-right-top -right-72 xl:-right-28"></div>
+//         </div>
+//         <Hero />
+//         <Motivation />
+//       </div>
+//       <Blog />
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default Home;
